5bd3969ed6e77e363a63e15900219bc1602b2638
SoyDina <tomas.dina98@gmail.com>
2021-06-27 17:58:49 -0300

    SVM PCA random forest NN

---
+++ neural_network.py
@@ -0,0 +1,13 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.neural_network import MLPClassifier
+from keras.models import Sequential, load_model
+#from keras.layers.core import Dense, Dropout, Activation
+#from keras.utils import np_utils, plot_model
+#from keras.regularizers import l1
+
+print("hola")
